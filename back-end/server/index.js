3b16fd01f1d6d5da326fdc70130759a2c0b20143
halwilcox <halwilcox09@gmail.com>
2021-09-17 14:41:33 -0600

    Im not finished yet

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -9,9 +9,11 @@ app.use(cors());
 app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
+  const compliments = ["A pleasant surprise is waiting for you.",
+					 "A hunch is creativity trying to tell you something.",
+					 "All your hard work will soon pay off.",
+           "A person of words and not deeds is like a garden full of weeds",
+           "Be careful or you could fall for some tricks today.",
   ];
 
   // choose random compliment
@@ -19,7 +21,26 @@ app.get("/api/compliment", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-  
+});
+
+let user = {
+  username: 'Dragon',
+  firstName: 'Joey',
+  lastName: 'Tribiani'
+}
+
+app.post('/api/users', (req,res) => {
+  console.log(req.body)
+  let username = req.body.username
+  let firstName = req.body.firstName
+  let lastName = req.body.lastName
+  res.status(200).send(`Welcome, ${username}! It is nice to meet you ${firstName} ${lastName}.`)
+})
+
+app.get("/api/users", (req, res) => {
+  let family = ["Adelaide", "Mitchell", "Wyatt", "Ranger", "Bowie"];
+  res.status(200).send(family);
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
+
